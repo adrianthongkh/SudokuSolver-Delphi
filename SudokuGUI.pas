8b947d7e07781a2996f4acef99f52874c720f82b
Adrian <adrian_thong@outlook.com>
2021-10-27 10:51:59 +0800

    Added Multi-Threading

--- SudokuGUI.pas
+++ SudokuGUI.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SudokuSolver, Vcl.Grids, Vcl.StdCtrls, System.Diagnostics,
-  System.TimeSpan;
+  System.TimeSpan, Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
@@ -13,20 +13,31 @@ type
     BtnSolve: TButton;
     BtnClear: TButton;
     TimeElapsed: TStaticText;
+    Panel1: TPanel;
     procedure FormCreate(Sender: TObject);
     procedure BtnSolveClick(Sender: TObject);
     procedure BtnClearClick(Sender: TObject);
     procedure DisplaySolution;
     procedure ClearGrid;
   private
-    Stopwatch: TStopWatch;
-    Elapsed: TTimeSpan;
   public
     { Public declarations }
   end;
 
+  MyThread = class(TThread)
+    private
+    protected
+    public
+      constructor Create;
+      destructor Destroy; override;
+      procedure Execute; override;
+  end;
+
 var
   Form2: TForm2;
+  Stopwatch: TStopWatch;
+  Elapsed: TTimeSpan;
+  MT: MyThread;
 
 implementation
 
@@ -40,11 +51,13 @@ end;
 
 procedure TForm2.DisplaySolution;
 begin
+  StringGrid.DefaultDrawing := True;
   for var I := 0 to 8 do begin
-    for var J := 0 to 8 do begin
+    StringGrid.DefaultDrawing := True;for var J := 0 to 8 do begin
       StringGrid.Cells[J, I] := IntToStr(SudokuGrid[I+1][J+1]);
     end;
   end;
+  Panel1.Visible := False;
 end;
 
 procedure TForm2.ClearGrid;
@@ -74,17 +87,36 @@ begin
 
   if not IsValid then ShowMessage('Given Sudoku is not valid.')
   else begin
-    Stopwatch := TStopWatch.StartNew;
-    SolveSudoku;
-    DisplaySolution;
-    Elapsed := Stopwatch.Elapsed;
-    TimeElapsed.Caption := 'Time Taken to solve: '+ sLineBreak + Elapsed.TotalSeconds.ToString + 's';
+    Panel1.Visible := True;
+    MT := MyThread.Create;
   end;
 end;
 
 procedure TForm2.BtnClearClick(Sender: TObject);
 begin
   ClearGrid;
+end;
+
+{ MyThread }
+
+constructor MyThread.Create;
+begin
+  inherited Create;
+end;
+
+destructor MyThread.Destroy;
+begin
+  inherited;
+end;
+
+procedure MyThread.Execute;
+begin
+  Stopwatch := TStopWatch.StartNew;
+  Form2.StringGrid.DefaultDrawing := False;
+  SolveSudoku;
+  Form2.DisplaySolution;
+  Elapsed := Stopwatch.Elapsed;
+  Form2.TimeElapsed.Caption := 'Time Taken to solve: '+ sLineBreak + Elapsed.TotalSeconds.ToString + 's';
 end;
 
 end.
